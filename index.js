0855fc94a37d3a5cc4190ab0a8c538bbbb49967a
Phil <philrsheridan@gmail.com>
2023-09-28 16:32:22 -0400

    passed 2 tests (?)

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record) {
+    const isWin = function(element) {
+        if (element.result === 'W') {
+            return element.year;
+        }else{
+            return null;
+        }
+    }
+    let test = record.find(isWin);
+    if (test != null) {
+        return test.year
+    } else {
+        return undefined;
+    }
+}
